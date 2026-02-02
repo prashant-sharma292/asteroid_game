7fa648b03aea402ea3e1c207044204d68f41d5ec
Prashant Sharma <prashant@proactai.in>
2026-02-02 14:25:42 +0530

    limit game to 60 fps using delta time

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +17,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 # pauses the game loop until 1/60th of second
+
 
 
 if __name__ == "__main__":
