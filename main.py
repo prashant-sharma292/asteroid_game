c6621a120a96f2f79b1ca2df5d618632a3ca09a7
Prashant Sharma <prashant@proactai.in>
2026-02-05 17:27:57 +0530

    feat: use sprite groups for updating and drawing player

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
@@ -18,8 +21,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)   
         pygame.display.flip()
         dt = clock.tick(60) / 1000 # pauses the game loop until 1/60th of second
 
