f6204597add7888e3a516f5868dfddbc252eea3e
Prashant Sharma <prashant@proactai.in>
2026-02-06 20:45:03 +0530

    feat: asteriods destroyed when shot by bullet

--- main.py
+++ main.py
@@ -37,6 +37,11 @@ def main():
                 log_event("player_hit")
                 print("Game Over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
         for sprite in drawable:
             sprite.draw(screen)   
         pygame.display.flip()
