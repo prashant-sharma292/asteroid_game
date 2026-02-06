b5a8a15e62652e287af56c651fe0e3b8b33b6c55
Prashant Sharma <prashant@proactai.in>
2026-02-06 21:27:44 +0530

    feat: bigger asteroid when destroyed creates smaller asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 import pygame
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -13,4 +15,16 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
-        
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        new_angle = random.uniform(20, 50)
+        asteroid_1 = self.velocity.rotate(new_angle)
+        asteroid_2 = self.velocity.rotate(-new_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_1.velocity = asteroid_1 * 1.2
+        new_asteroid_2.velocity = asteroid_2 * 1.2
